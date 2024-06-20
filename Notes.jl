e35e68925d8b8ca17eb4cd2155fd6c9ebfed2b6e
Rayan Bin Elias <bineliasmrb@realve.io>
2024-06-20 17:07:24 +0800

    added 20th June 2024 progress

---
+++ Notes.jl
@@ -0,0 +1,5 @@
+CKMS
+  Separation of duties:
+    - you could run Cloud KMS in its own projec
+    - Create your-key-project without an owner at the project level
+    - and designate an Organization Admin granted at the organization-level.
